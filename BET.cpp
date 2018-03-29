32602337a2a4c8fb6e3d1b93686fbdd47a2fbcf6
technicalmillenial <klk16e@my.fsu.edu>
2018-03-29 13:26:59 -0400

    Update BET.cpp

--- BET.cpp
+++ BET.cpp
@@ -1,6 +1,6 @@
 #include "BET.h"
 #include <iostream>
-#include <stream>
+#include <stack>
 
 using namespace std;
 
@@ -23,5 +23,36 @@ BET::~BET()
 
 bool buildFromPostfix(const string postfix)
 {
- 
+ stack<BinaryNode*> theStack;
+  
+ if(!theStack.empty()){
+  makeEmpty();
+ }
+  else
+  {
+    for(int i=0; i<postfix.length(); i++)
+    { //if isoperand then create a one-node tree and push a pointer to it onto a stack
+      if(isalnum(postfix[i]))
+      {
+        theStack.push(postfix[i]).
+      }
+      //if isoperator then pop pointers to the left and right nodes from the stack and then a pointer is then pushed onto the stack
+      else if((postfix[i] == '+')||(postfix[i] == '-')||(postfix[i] == '*')||(postfix[i] == '/'))
+      {
+        BinaryNode* Rnode = theStack.top();
+        theStack.pop()
+        BinaryNode* Lnode = theStack.top();
+        theStack.pop();
+        BinaryNode* ptr = new BinaryNode(postfix[i], Lnode, Rnode);
+        theStack.push(ptr);
+      }
+    }
+    if(theStack.empty()){
+      cout<<"Error. New tree unsuccessful.\n";
+      return false;
+    }
+    else
+      cout<<"Success! New tree built!\n";
+      return true;
+  }
 }
