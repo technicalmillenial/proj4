6c7c4d7fb22262dfd5c9ad32b8d5a274b58f486a
technicalmillenial <klk16e@my.fsu.edu>
2018-04-03 20:07:36 -0400

    buildFromPostfix
    needs review

--- BET.cpp
+++ BET.cpp
@@ -18,7 +18,7 @@ BET::BET(const BET& b) : root{nullptr}
 
 BET::~BET()
 {
- makeEmpty();
+ makeEmpty(root);
 }
 
 bool BET::buildFromPostfix(const string postfix)
@@ -26,28 +26,28 @@ bool BET::buildFromPostfix(const string postfix)
  stack<BinaryNode*> theStack;
   
  if(!theStack.empty()){
-  makeEmpty();
+  makeEmpty(root);
  }
   else
   {
-    for(int i=0; i<postfix.length(); i++)
+    for(size_t i=0; i<postfix.length(); i++)
     { //if isoperand then create a one-node tree and push a pointer to it onto a stack
-      if(isalnum(postfix[i]))
+      if(((postfix[i] != '+')||(postfix[i] != '-')||(postfix[i] != '*')||(postfix[i] != '/'))&&(!theStack.empty()))
       {
-        theStack.push(postfix[i]).
+        theStack.push(postfix[i]);
       }
       //if isoperator then pop pointers to the left and right nodes from the stack and then a pointer is then pushed onto the stack
-      else if((postfix[i] == '+')||(postfix[i] == '-')||(postfix[i] == '*')||(postfix[i] == '/'))
+      else if(((postfix[i] == '+')||(postfix[i] == '-')||(postfix[i] == '*')||(postfix[i] == '/'))&&(!theStack.empty()))
       {
         BinaryNode* Rnode = theStack.top();
-        theStack.pop()
+        theStack.pop();
         BinaryNode* Lnode = theStack.top();
         theStack.pop();
         BinaryNode* ptr = new BinaryNode(postfix[i], Lnode, Rnode);
         theStack.push(ptr);
       }
     }
-    if(theStack.empty()){
+    else if(theStack.empty()){
       cout<<"Error. New tree unsuccessful.\n";
       return false;
     }
@@ -56,7 +56,7 @@ bool BET::buildFromPostfix(const string postfix)
       return true;
   }
 }
-const BET::BET& operator=(const BET &b)
+const BET& BET::operator=(const BET &b)
 {
   root = clone(b.root);
   return *this;
@@ -113,7 +113,7 @@ BinaryNode* BET::clone(BinaryNode *t)const
     {   if(t==nullptr)
           {return nullptr;}
          else
-        return new BinaryNode{t->element, clone(t->left), clone(t->right)};
+        return new BinaryNode{t->token, clone(t->left), clone(t->right)};
     }  //clone all nodes in subtree pointed to by t
     //called by assignment operator=
     
